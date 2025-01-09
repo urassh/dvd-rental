eef081e0930b8824c07e639a0e468a5b48a17d3b
urassh <shuto.1011@icloud.com>
2025-01-09 23:32:23 +0900

    feat: sidebar

--- src/main/java/com/urassh/dvdrental/controller/SidebarController.java
+++ src/main/java/com/urassh/dvdrental/controller/SidebarController.java
@@ -1,4 +1,34 @@
 package com.urassh.dvdrental.controller;
 
+import com.urassh.dvdrental.util.Navigator;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class SidebarController {
+    @FXML
+    private Label title;
+
+    @FXML
+    protected void navigateToRentalButton() {
+        Navigator navigator = new Navigator(title.getScene());
+        navigator.navigateToRental();
+    }
+
+    @FXML
+    protected void navigateToMembersButton() {
+        Navigator navigator = new Navigator(title.getScene());
+        navigator.navigateToMembers();
+    }
+
+    @FXML
+    protected void navigateToGoodsButton() {
+        Navigator navigator = new Navigator(title.getScene());
+        navigator.navigateToGoods();
+    }
+
+    @FXML
+    protected void navigateToReturnButton() {
+        Navigator navigator = new Navigator(title.getScene());
+        navigator.navigateToReturn();
+    }
 }
