6a49c10dc2f4ae753a093488ed27dc98f85e929c
urassh <shuto.1011@icloud.com>
2025-01-14 13:31:52 +0900

    feat: とりあえずListでMemberが一覧で表示できるようにした

---
+++ src/main/java/com/urassh/dvdrental/controller/members/MemberCell.java
@@ -0,0 +1,35 @@
+package com.urassh.dvdrental.controller.members;
+
+import com.urassh.dvdrental.RentalApp;
+import com.urassh.dvdrental.domain.Member;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.HBox;
+
+import java.io.IOException;
+
+public class MemberCell extends ListCell<Member> {
+    private HBox content;
+    private MemberCellController controller;
+
+    public MemberCell() {
+        try {
+            FXMLLoader loader = new FXMLLoader(RentalApp.class.getResource("members/member_cell/component.fxml"));
+            content = loader.load();
+            controller = loader.getController();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void updateItem(Member member, boolean empty) {
+        super.updateItem(member, empty);
+        if (empty || member == null) {
+            setGraphic(null);
+        } else {
+            controller.setMember(member);
+            setGraphic(content);
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,19 +2,16 @@ module com.urassh.dvdrental {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens com.urassh.dvdrental to javafx.fxml;
     exports com.urassh.dvdrental;
-    exports com.urassh.dvdrental.controller;
-    opens com.urassh.dvdrental.controller to javafx.fxml;
     exports com.urassh.dvdrental.controller.goods;
-    opens com.urassh.dvdrental.controller.goods to javafx.fxml;
     exports com.urassh.dvdrental.controller.home;
-    opens com.urassh.dvdrental.controller.home to javafx.fxml;
     exports com.urassh.dvdrental.controller.members;
-    opens com.urassh.dvdrental.controller.members to javafx.fxml;
     exports com.urassh.dvdrental.controller.rental;
-    opens com.urassh.dvdrental.controller.rental to javafx.fxml;
     exports com.urassh.dvdrental.controller.sidebar;
+
+    opens com.urassh.dvdrental.controller.goods to javafx.fxml;
+    opens com.urassh.dvdrental.controller.home to javafx.fxml;
+    opens com.urassh.dvdrental.controller.members to javafx.fxml;
+    opens com.urassh.dvdrental.controller.rental to javafx.fxml;
     opens com.urassh.dvdrental.controller.sidebar to javafx.fxml;
 }
