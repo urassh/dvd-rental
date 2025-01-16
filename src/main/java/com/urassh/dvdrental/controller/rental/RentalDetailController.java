55b7a893cd5219563e73fdd52ca89d47ce2b409e
urassh <shuto.1011@icloud.com>
2025-01-16 22:29:34 +0900

    feat: カートに追加したらカート数がカウントされるようにした

--- src/main/java/com/urassh/dvdrental/controller/rental/RentalDetailController.java
+++ src/main/java/com/urassh/dvdrental/controller/rental/RentalDetailController.java
@@ -1,6 +1,7 @@
 package com.urassh.dvdrental.controller.rental;
 
 import com.urassh.dvdrental.domain.Goods;
+import com.urassh.dvdrental.usecase.rental.AddToRentalCartUseCase;
 import com.urassh.dvdrental.util.Navigator;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -29,7 +30,10 @@ public class RentalDetailController {
     @FXML
     private Label isNewLabel;
 
+    private Goods goods;
+
     public void setGoods(Goods goods) {
+        this.goods = goods;
         final String GOODS_DATE = new SimpleDateFormat("yyyy-MM-dd").format(goods.getReleaseDate());
         titleLabel.setText(goods.getTitle());
         idLabel.setText(goods.getId());
@@ -41,6 +45,7 @@ public class RentalDetailController {
     }
 
     public void onAddToCart() {
+        new AddToRentalCartUseCase().execute(goods);
         Navigator navigator = new Navigator(titleLabel.getScene());
 
         navigator.navigateToRental();
