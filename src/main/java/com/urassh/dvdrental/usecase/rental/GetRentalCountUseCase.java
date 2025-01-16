1905b4c08e06371f6a83da2adda9cc8681ffc67a
urassh <shuto.1011@icloud.com>
2025-01-16 22:13:40 +0900

    feat: GetRentalCountUseCase.java

--- src/main/java/com/urassh/dvdrental/usecase/rental/GetRentalCountUseCase.java
+++ src/main/java/com/urassh/dvdrental/usecase/rental/GetRentalCountUseCase.java
@@ -1,4 +1,11 @@
 package com.urassh.dvdrental.usecase.rental;
 
+import com.urassh.dvdrental.infrastructure.LocalStore;
+
 public class GetRentalCountUseCase {
+    private final LocalStore localStore = new LocalStore();
+
+    public int execute() {
+        return localStore.getRentalCount();
+    }
 }
