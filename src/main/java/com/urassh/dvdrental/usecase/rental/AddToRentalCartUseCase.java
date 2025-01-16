a26845574b4c490bec4077e113d75329bfe82b6b
urassh <shuto.1011@icloud.com>
2025-01-16 22:13:30 +0900

    feat: AddToRentalCartUseCase.java

--- src/main/java/com/urassh/dvdrental/usecase/rental/AddToRentalCartUseCase.java
+++ src/main/java/com/urassh/dvdrental/usecase/rental/AddToRentalCartUseCase.java
@@ -1,4 +1,13 @@
 package com.urassh.dvdrental.usecase.rental;
 
+import com.urassh.dvdrental.domain.Goods;
+import com.urassh.dvdrental.infrastructure.LocalStore;
+
 public class AddToRentalCartUseCase {
+    private final LocalStore localStore = new LocalStore();
+
+    public void execute(Goods goods) {
+        int count = localStore.getRentalCount();
+        localStore.setRentalCount(count + 1);
+    }
 }
