20a4278758f77588ac52a531ea0465041c759754
earcra0106 <haji1003@outlook.jp>
2025-01-17 00:24:04 +0900

    feat: 商品を追加ボタンの遷移実装

--- src/main/java/com/urassh/dvdrental/controller/goods/GoodsController.java
+++ src/main/java/com/urassh/dvdrental/controller/goods/GoodsController.java
@@ -1,4 +1,38 @@
 package com.urassh.dvdrental.controller.goods;
 
+import com.urassh.dvdrental.util.Navigator;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class GoodsController {
+    @FXML
+    private Label title;
+
+    @FXML
+    private Button newGoodsButton;
+
+    @FXML
+    protected void initialize() {
+        newGoodsButton.setOnAction(event -> handleButtonClick(newGoodsButton));
+    }
+
+    private void handleButtonClick(Button clickedButton) {
+        clickedButton.getStyleClass().remove("active");
+
+        clickedButton.getStyleClass().add("active");
+
+        String buttonId = clickedButton.getId();
+
+        navigateTo(buttonId);
+    }
+
+    private void navigateTo(String buttonId) {
+        Navigator navigator = new Navigator(title.getScene());
+        switch (buttonId) {
+            case "newGoodsButton":
+                navigator.navigateToNewGoods();
+                break;
+        }
+    }
 }
---
+++ src/main/java/com/urassh/dvdrental/controller/goods/NewGoodsController.java
@@ -0,0 +1,38 @@
+package com.urassh.dvdrental.controller.goods;
+
+import com.urassh.dvdrental.util.Navigator;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class NewGoodsController {
+    @FXML
+    private Label title;
+
+    @FXML
+    protected void initialize() {
+//        newGoodsButton.setOnAction(event -> handleButtonClick(newGoodsButton));
+    }
+
+    private void handleButtonClick(Button clickedButton) {
+//        System.out.println("ボタンが押された");
+//        clickedButton.getStyleClass().remove("active");
+//
+//        clickedButton.getStyleClass().add("active");
+//
+//        String buttonId = clickedButton.getId();
+//
+//        navigateTo(buttonId);
+    }
+
+    private void navigateTo(String buttonId) {
+//        System.out.println("ナビゲートが発動");
+//        Navigator navigator = new Navigator(title.getScene());
+//        switch (buttonId) {
+//            case "newGoodsButton":
+//                System.out.println("画面を呼び出す");
+//                navigator.navigateToNewGoods();
+//                break;
+//        }
+    }
+}
