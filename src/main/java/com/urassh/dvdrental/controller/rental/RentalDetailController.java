b2ce4ff27f5d8da08e3ebf15bafc8f449b2f2adc
urassh <shuto.1011@icloud.com>
2025-01-16 21:13:59 +0900

    feat: 貸し出し画面の商品詳細実装

--- src/main/java/com/urassh/dvdrental/controller/rental/RentalDetailController.java
+++ src/main/java/com/urassh/dvdrental/controller/rental/RentalDetailController.java
@@ -4,6 +4,8 @@ import com.urassh.dvdrental.domain.Goods;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.text.SimpleDateFormat;
+
 public class RentalDetailController {
     @FXML
     private Label titleLabel;
@@ -11,8 +13,29 @@ public class RentalDetailController {
     @FXML
     private Label idLabel;
 
+    @FXML
+    private Label genreLabel;
+
+    @FXML
+    private Label releaseDateLabel;
+
+    @FXML
+    private Label rentalCountLabel;
+
+    @FXML
+    private Label belongToStoreLabel;
+
+    @FXML
+    private Label isNewLabel;
+
     public void setGoods(Goods goods) {
+        final String GOODS_DATE = new SimpleDateFormat("yyyy-MM-dd").format(goods.getReleaseDate());
         titleLabel.setText(goods.getTitle());
         idLabel.setText(goods.getId());
+        genreLabel.setText(goods.getGenre());
+        releaseDateLabel.setText(GOODS_DATE);
+        rentalCountLabel.setText(String.valueOf(goods.getLoanCount()));
+        belongToStoreLabel.setText(goods.getBelongToStore());
+        isNewLabel.setText(goods.isNew() ? "新作" : "旧作");
     }
 }
