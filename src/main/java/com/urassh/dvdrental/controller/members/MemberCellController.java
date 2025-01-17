b62bd48ee508a555c4d024572214076e0c991f91
SORA-97 <sora.versatile@gmail.com>
2025-01-18 00:11:57 +0900

    会員一覧にアイコンを追加

--- src/main/java/com/urassh/dvdrental/controller/members/MemberCellController.java
+++ src/main/java/com/urassh/dvdrental/controller/members/MemberCellController.java
@@ -3,6 +3,9 @@ package com.urassh.dvdrental.controller.members;
 import com.urassh.dvdrental.domain.Member;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
 
 public class MemberCellController {
     @FXML
@@ -10,9 +13,18 @@ public class MemberCellController {
 
     @FXML
     private Label memberNameLabel;
+    
+    @FXML
+    private ImageView memberImageView;
 
     public void setMember(Member member) {
         memberIdLabel.setText(member.getId().toString());
         memberNameLabel.setText(member.getName() + " 様");
+        Image memberImage = new Image("file:src/main/resources/com/urassh/dvdrental/members/images/" + member.getId() + ".png");
+        memberImageView.setImage(memberImage);
+        Circle circle = new Circle(30);
+        circle.setCenterX(30);
+        circle.setCenterY(30);
+        memberImageView.setClip(circle);
     }
 }
