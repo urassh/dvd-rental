3465c8811e46cf32c85ff5667c3fb634f90fb773
urassh <shuto.1011@icloud.com>
2025-01-17 16:46:07 +0900

    fix: GetRentingMemberUseCase

--- src/main/java/com/urassh/dvdrental/usecase/rental/GetRentingMemberUseCase.java
+++ src/main/java/com/urassh/dvdrental/usecase/rental/GetRentingMemberUseCase.java
@@ -1,6 +1,5 @@
 package com.urassh.dvdrental.usecase.rental;
 
-import com.urassh.dvdrental.domain.Goods;
 import com.urassh.dvdrental.domain.Member;
 import com.urassh.dvdrental.domain.Rental;
 import com.urassh.dvdrental.domain.interfaces.MemberRepository;
