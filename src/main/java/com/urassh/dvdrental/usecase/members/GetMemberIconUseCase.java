cb399f2b53d003e3b698c331d111363f1580ec49
urassh <shuto.1011@icloud.com>
2025-01-21 12:49:43 +0900

    fix: default image

--- src/main/java/com/urassh/dvdrental/usecase/members/GetMemberIconUseCase.java
+++ src/main/java/com/urassh/dvdrental/usecase/members/GetMemberIconUseCase.java
@@ -1,11 +1,9 @@
 package com.urassh.dvdrental.usecase.members;
 
-import com.google.inject.Inject;
 import com.urassh.dvdrental.domain.Member;
 import com.urassh.dvdrental.domain.interfaces.ImageRepository;
 import com.urassh.dvdrental.infrastructure.dummy.ImageDummyRepository;
 import javafx.scene.image.Image;
-
 import java.io.ByteArrayInputStream;
 
 public class GetMemberIconUseCase {
