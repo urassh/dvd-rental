3ec4d3d69b8fd62d20ee5998eb2b648dca068bb6
urassh <shuto.1011@icloud.com>
2025-01-31 12:40:53 +0900

    feat: repository impl

--- src/main/java/com/urassh/dvdrental/module/GoodsModule.java
+++ src/main/java/com/urassh/dvdrental/module/GoodsModule.java
@@ -2,11 +2,22 @@ package com.urassh.dvdrental.module;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
+import com.urassh.dvdrental.domain.interfaces.GoodsRepository;
+import com.urassh.dvdrental.infrastructure.gateway.GoodsGatewayImpl;
+import com.urassh.dvdrental.repository.dummy.GoodsDummyRepository;
+import com.urassh.dvdrental.repository.interfaces.GoodsGateway;
 import com.urassh.dvdrental.usecase.goods.*;
 
 public class GoodsModule extends AbstractModule {
     @Override
     protected void configure() {
+        // Gateway
+        bind(GoodsGateway.class).to(GoodsGatewayImpl.class).in(Singleton.class);
+
+        // Repository
+        bind(GoodsRepository.class).to(GoodsDummyRepository.class).in(Singleton.class);
+
+        // UseCase
         bind(AddGoodsUseCase.class).in(Singleton.class);
         bind(DeleteGoodsUseCase.class).in(Singleton.class);
         bind(GetAllGoodsUseCase.class).in(Singleton.class);
--- src/main/java/com/urassh/dvdrental/module/RentalModule.java
+++ src/main/java/com/urassh/dvdrental/module/RentalModule.java
@@ -2,6 +2,8 @@ package com.urassh.dvdrental.module;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
+import com.urassh.dvdrental.domain.interfaces.RentalRepository;
+import com.urassh.dvdrental.repository.dummy.RentalDummyRepository;
 import com.urassh.dvdrental.usecase.rental.AddRentalUseCase;
 import com.urassh.dvdrental.usecase.rental.GetRentalsByMemberUseCase;
 import com.urassh.dvdrental.usecase.returns.ReturnUseCase;
@@ -9,6 +11,10 @@ import com.urassh.dvdrental.usecase.returns.ReturnUseCase;
 public class RentalModule extends AbstractModule {
     @Override
     protected void configure() {
+        // Repository
+        bind(RentalRepository.class).to(RentalDummyRepository.class).in(Singleton.class);
+
+        // UseCase
         bind(AddRentalUseCase.class).in(Singleton.class);
         bind(GetRentalsByMemberUseCase.class).in(Singleton.class);
         bind(ReturnUseCase.class).in(Singleton.class);
